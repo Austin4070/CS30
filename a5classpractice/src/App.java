bfeec2289d4439dfd578f1e6cc763e9b8a3de8cc
Austin4070 <austin.dimen@rcsd.ca>
2024-03-22 12:06:08 -0600

    finished A5

---
+++ a5classpractice/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Computer Science 30");
+        System.out.println("My Name is Austin and my favourite food is chicken.");
+        System.out.println("Java has all sorta of weird \"quirks\" which make displaying text a bit more challenging.");
+        System.out.println("Sometimes characters like parentheses (sometimes called brackets) don't work correctly.");
+        System.out.println("Escape sequences require us to use a backslash '\\' in order to output these special characters.");
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    }
+}
