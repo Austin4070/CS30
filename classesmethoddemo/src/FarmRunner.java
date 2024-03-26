fad45d0fa443f8a9b56896a84be232c6b6164fc2
Austin4070 <austin.dimen@rcsd.ca>
2024-03-26 11:51:57 -0600

    oink oinked

---
+++ classesmethoddemo/src/FarmRunner.java
@@ -0,0 +1,23 @@
+public class TurkeyRunner {
+    public static void main(String[] args) throws Exception {
+        Turkey bird = new Turkey();
+        bird.speak();
+        bird.sayName();
+        bird.speak();
+        bird.sayName();
+        bird.speak();
+        bird.sayName();
+        bird.speak();
+        bird.sayName();
+        bird.speak();
+        bird.sayName();
+        bird.speak();
+        bird.sayName();
+        bird.speak();
+        bird.sayName();
+        
+    
+    
+    
+    }
+}
