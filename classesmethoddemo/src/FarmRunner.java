2ae8bb533d324f0b346732c13d4433217fe25709
Austin4070 <austin.dimen@rcsd.ca>
2024-03-26 12:11:01 -0600

    pig go oink

--- classesmethoddemo/src/FarmRunner.java
+++ classesmethoddemo/src/FarmRunner.java
@@ -1,20 +1,14 @@
-public class TurkeyRunner {
+public class FarmRunner {
     public static void main(String[] args) throws Exception {
         Turkey bird = new Turkey();
+        Pig pig = new Pig();
+        
+        
+        
         bird.speak();
         bird.sayName();
         bird.speak();
-        bird.sayName();
-        bird.speak();
-        bird.sayName();
-        bird.speak();
-        bird.sayName();
-        bird.speak();
-        bird.sayName();
-        bird.speak();
-        bird.sayName();
-        bird.speak();
-        bird.sayName();
+        pig.speak();
         
     
     
