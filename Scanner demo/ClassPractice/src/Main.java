402a12c1d9d59a8806538d2cd3314b15ec0b6c3a
Austin4070 <austin.dimen@rcsd.ca>
2024-03-27 11:28:26 -0600

    ClassPractice

---
+++ Scanner demo/ClassPractice/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Austin"; // 
+        String food = "chicken"; // 
+
+        System.out.println("Welcome to Computer Science 30");
+        System.out.println("My Name is " + name + " and my favourite food is " + food + ".");
+        System.out.println("Java has all sorta of weird \"quirks\" which make displaying text a bit more challenging.");
+        System.out.println("Sometimes characters like parentheses (sometimes called brackets) don't work correctly.");
+        System.out.println("Escape sequences require us to use a backslash '\\' in order to output these special characters.");
+    }
+}
