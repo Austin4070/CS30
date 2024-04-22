0c09d3ab0c1117c0e1b5b78c266af3ce63abbde0
Austin4070 <austin.dimen@rcsd.ca>
2024-04-22 11:25:07 -0600

    FinishedPassingMethods

---
+++ Scanner demo/PassingMethods/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Fox fox = new Fox();
+        Kitten garfield = new Kitten(); 
+
+
+        fox.speak();
+        garfield.speak();
+
+
+
+
+
+
+}
+}
---
+++ Scanner demo/PassingMethods/src/Kitten.java
@@ -0,0 +1,5 @@
+public class Kitten {
+    public void speak() {
+        System.out.println("Meow");
+    }
+}
